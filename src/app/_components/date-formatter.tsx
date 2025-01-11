9d4f9b4a9df943695b6f9a4da9041717be6f8c32
Alessandro Miracapillo <alesim11@gmail.com>
2025-01-11 14:11:33 +0100

    fix: Error: dateString.split is not a function

--- src/app/_components/date-formatter.tsx
+++ src/app/_components/date-formatter.tsx
@@ -1,12 +1,12 @@
 import { parseISO, format } from "date-fns";
 
 type Props = {
-  dateString: string;
+  dateString: string | Date; // Allow dateString to be a Date object as well
 };
 
 const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+  const date = typeof dateString === 'string' ? parseISO(dateString) : dateString; // Check if dateString is a string
+  return <time dateTime={dateString instanceof Date ? dateString.toISOString() : dateString}>{format(date, "LLLL d, yyyy")}</time>;
 };
 
 export default DateFormatter;
